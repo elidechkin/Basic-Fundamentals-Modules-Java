78be4d5a0a91506b6afbe571edab4679e5f677e5
elideckin <familia_georgievi@yahoo.com>
2022-06-02 22:24:29 +0300

    update

--- src/Array/EqualArrays.java
+++ src/Array/EqualArrays.java
@@ -1,9 +1,36 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EqualArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] firstArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] secondArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        boolean notIndentical = false;
+        for (int i = 0; i < firstArr.length ; i++) {
+            sum += firstArr[i];
+
+            if (firstArr[i] != secondArr[i]){
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                notIndentical = true;
+                break;
+            }
+        }
+        if (!notIndentical){
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        }
+
     }
 }
