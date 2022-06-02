7bd78543bba625d92abbb746142f466154a4ef12
elideckin <familia_georgievi@yahoo.com>
2022-06-02 22:31:31 +0300

    update

---
+++ src/Array/CondenseArraytoNumber.java
@@ -0,0 +1,10 @@
+package Array;
+
+import java.util.Scanner;
+
+public class CondenseArraytoNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
