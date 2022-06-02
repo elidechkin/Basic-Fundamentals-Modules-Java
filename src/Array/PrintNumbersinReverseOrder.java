5f5637dcc94cde1b87012ca2eb0a9ee8a672c28b
elideckin <familia_georgievi@yahoo.com>
2022-06-02 18:01:57 +0300

    update

--- src/Array/PrintNumbersinReverseOrder.java
+++ src/Array/PrintNumbersinReverseOrder.java
@@ -6,5 +6,19 @@ public class PrintNumbersinReverseOrder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] numbersArr = new int[n];
+
+        for (int i = 0; i < numbersArr.length; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            numbersArr[i] = number;
+        }
+        for (int i = numbersArr.length - 1; i >= 0 ; i--) {
+            System.out.print(numbersArr[i] + " ");
+
+        }
+
+
     }
 }
---
+++ src/Array/ReverseArrayofStrings.java
@@ -0,0 +1,10 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ReverseArrayofStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
