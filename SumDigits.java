4ad8e2a836b313bdf3c81590c03070f2814adab9
elideckin <familia_georgievi@yahoo.com>
2022-06-02 20:45:50 +0300

    update

---
+++ SumDigits.java
@@ -0,0 +1,14 @@
+package DataTypeVariables;
+
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.next());
+        
+
+
+    }
+}
