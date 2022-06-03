c60678f98f4933da7261124d5ae26b2e1f00fde3
elideckin <familia_georgievi@yahoo.com>
2022-06-03 07:42:24 +0300

    update

--- src/Array/CondenseArraytoNumber.java
+++ src/Array/CondenseArraytoNumber.java
@@ -1,10 +1,26 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CondenseArraytoNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        while (input.length > 1) {
+            int[] condensed = new int[input.length - 1];
+
+            for (int i = 0; i < condensed.length; i++) {
+                condensed[i] = input[i] + input[i + 1];
+            }
+            input = condensed;
+        }
+        System.out.println(input[0]);
 
     }
 }
