bc17ce6a2655cacf45cf4d39a374c7a830378ce0
elideckin <familia_georgievi@yahoo.com>
2022-06-09 17:48:07 +0300

    update

---
+++ src/Methods/Calculation.java
@@ -0,0 +1,40 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Calculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String action = scanner.nextLine();
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        switch (action){
+            case "add":
+                printAdd(a,b);
+                break;
+            case "multiply":
+                printMultiply(a,b);
+                break;
+            case "subtract":
+                printSubtract(a,b);
+                break;
+            case "divide":
+                printDivide(a,b);
+                break;
+        }
+    }
+    public  static void printAdd(int a, int b){
+        System.out.println(a + b);
+    }
+    public  static void printMultiply(int a, int b){
+        System.out.println(a * b);
+    }
+    public  static void printSubtract(int a, int b){
+        System.out.println(a - b);
+    }
+    public  static void printDivide(int a, int b){
+        System.out.println(a / b);
+    }
+}
