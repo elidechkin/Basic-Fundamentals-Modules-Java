16e4d48de0d9d9529f97e6422d4f7cdf5e0a1ef8
elideckin <familia_georgievi@yahoo.com>
2022-06-09 18:07:01 +0300

    update

---
+++ src/Methods/CalculateRectangleArea.java
@@ -0,0 +1,10 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
