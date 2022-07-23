5e413300fcc3eb22f936f9cd529c9ba0c2e792b1
elideckin <familia_georgievi@yahoo.com>
2022-07-23 16:04:36 +0300

    update

---
+++ src/RegularExpressions/MatchPhoneNumber.java
@@ -0,0 +1,27 @@
+package RegularExpressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b");
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> phoneNumbersList = new ArrayList<>();
+        while (matcher.find()){
+            String phoneNumber = matcher.group();
+
+            phoneNumbersList.add(phoneNumber);
+        }
+
+        System.out.println(String.join(", ", phoneNumbersList));
+    }
+}
---
+++ src/TrainingFundamentals/ThePianist.java
@@ -0,0 +1,10 @@
+package TrainingFundamentals;
+
+import java.util.Scanner;
+
+public class ThePianist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
