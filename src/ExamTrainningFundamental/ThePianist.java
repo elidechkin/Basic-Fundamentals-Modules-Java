7fba79e207988de29d8cdfa9444d94bc2d9add16
elideckin <familia_georgievi@yahoo.com>
2022-07-31 12:18:07 +0300

    update

--- src/ExamTrainningFundamental/ThePianist.java
+++ src/ExamTrainningFundamental/ThePianist.java
@@ -22,13 +22,51 @@ public class ThePianist {
             pieceInfo.add(tone);
 
             piecesMap.put(pieceName, pieceInfo);
+
         }
         String command = scanner.nextLine();
         while (!command.equals("Stop")){
             if (command.contains("Add")){
+            String pieceName = command.split("\\|")[1];
+            String composer = command.split("\\|")[2];
+            String tone = command.split("\\|")[3];
+            if (piecesMap.containsKey(pieceName)){
+                System.out.println(pieceName + " is already in the collection!");
+            }else {
+                List<String> newPiece = new ArrayList<>();
+                newPiece.add(composer);
+                newPiece.add(tone);
 
+                piecesMap.put(pieceName, newPiece);
+                System.out.printf("%s by %s in %s added to the collection!%n", pieceName, composer, tone);
             }
+
+            }else if (command.contains("Remove")){
+                String removedPiece = command.split("\\|")[1];
+                if (piecesMap.containsKey(removedPiece)){
+                    piecesMap.remove(removedPiece);
+                    System.out.printf("Successfully removed %s!%n", removedPiece);
+                }else {
+                    System.out.printf("Invalid operation! %s does not exist in the collection.%n", removedPiece);
+                }
+            }else if (command.contains("ChangeKey")){
+                String pieceName = command.split("\\|")[1];
+                String newKey = command.split("\\|")[2];
+                if (!piecesMap.containsKey(pieceName)) {
+                    System.out.printf("Invalid operation! %s does not exist in the collection.%n", pieceName);
+                }else {
+                    List<String> currentInfo = piecesMap.get(pieceName);
+                    currentInfo.remove(1);
+                    currentInfo.add(newKey);
+                    piecesMap.put(pieceName, currentInfo);
+                    System.out.printf("Changed the key of %s to %s!%n", pieceName, newKey);
+                }
+            }
+            command = scanner.nextLine();
         }
+        piecesMap.entrySet()
+                .forEach(entry -> System.out.printf("%s -> Composer: %s, Key: %s%n", entry.getKey(), entry.getValue().
+                        get(0), entry.getValue().get(1)));
 
     }
 }
--- src/demo.java
+++ src/demo.java
@@ -5,53 +5,85 @@ import java.util.stream.IntStream;
 public class demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String stops = scanner.nextLine(); //спирки: "Hawai::Cyprys-Greece"
-        StringBuilder stopsBuilder = new StringBuilder(stops);
+        int countPieces = Integer.parseInt(scanner.nextLine()); //брой на пиесите
+        //пиеса -> списък с информация ([0] -> композитора, [1] -> тоналност)
+        Map<String, List<String>> piecesMap = new LinkedHashMap<>();
+        //информация за пиеса
+        for (int piece = 1; piece <= countPieces; piece++) {
+            String pieceData = scanner.nextLine();
+            //"{piece}|{composer}|{tone}"
+            String[] piecePart = pieceData.split("\\|"); //["{piece}", "{composer}", "{tone}"]
+            String pieceName = piecePart[0];
+            String composer = piecePart[1];
+            String tone = piecePart[2];
 
+            List<String> pieceInfo = new ArrayList<>();
+            pieceInfo.add(composer); //[0]
+            pieceInfo.add(tone); //[1]
+
+            piecesMap.put(pieceName, pieceInfo);
+        }
+
+        //получаваме команди
         String command = scanner.nextLine();
-        while (!command.equals("Travel")) {
-            //команда
-            if (command.contains("Add Stop")) {
-                //1. command = "Add Stop:7:Rome".split(":") -> ["Add Stop", "7", "Rome"]
-                int index = Integer.parseInt(command.split(":")[1]);
-                String stopName = command.split(":")[2];
-                //1. валидиране на индекс
-                if (isValidIndex(index, stopsBuilder)) {
-                    stopsBuilder.insert(index, stopName);
-                }
-            } else if (command.contains("Remove Stop")) {
-                //2. command = "Remove Stop:11:16".split(":") -> ["Remove Stop", "11", "16"]
-                int startIndex = Integer.parseInt(command.split(":")[1]);
-                int endIndex = Integer.parseInt(command.split(":")[2]);
+        while (!command.equals("Stop")) {
+            if (command.contains("Add")) {
+                //command = "Add|{piece}|{composer}|{key}".split("|") -> ["Add", "{piece}", "{composer}", "{key}"]
+                String pieceName = command.split("\\|")[1];
+                String composer = command.split("\\|")[2];
+                String tone = command.split("\\|")[3];
+                //има такава пиеса
+                if (piecesMap.containsKey(pieceName)) {
+                    System.out.println(pieceName + " is already in the collection!");
+                } else {
+                    //не съществува такава пиеса
+                    //пиеса -> списък с информация([0] -> composer, [1] -> tone)
+                    List<String> info = new ArrayList<>();
+                    info.add(composer);
+                    info.add(tone);
 
-                if (isValidIndex(startIndex, stopsBuilder) && isValidIndex(endIndex, stopsBuilder)) {
-                    stopsBuilder.delete(startIndex, endIndex + 1);
+                    piecesMap.put(pieceName, info);
+                    System.out.printf("%s by %s in %s added to the collection!%n", pieceName, composer, tone);
                 }
-            } else if (command.contains("Switch")) {
-                //3. command = "Switch:Hawai:Bulgaria".split(":") -> ["Switch", "Hawai", "Bulgaria"]
-                String oldStop = command.split(":")[1];
-                String newStop = command.split(":")[2];
-                //!!!!правим замяната само ако имаме такава спирка в първоначалния текст(stops)
-                if (stops.contains(oldStop)) {
-                    String updatedText = stopsBuilder.toString().replace(oldStop, newStop);
-                    stopsBuilder = new StringBuilder(updatedText);
+            } else if (command.contains("Remove")) {
+                //command = "Remove|{piece}".split("|") -> ["Remove", "{piece}"]
+                String removedPiece = command.split("\\|")[1];
+                //имаме такава пиеса
+                if (piecesMap.containsKey(removedPiece)) {
+                    piecesMap.remove(removedPiece);
+                    System.out.printf("Successfully removed %s!%n", removedPiece);
+                } else {
+                    //нямаме такава пиеса
+                    System.out.printf("Invalid operation! %s does not exist in the collection.%n", removedPiece);
+                }
+            } else if (command.contains("ChangeKey")) {
+                //command = "ChangeKey|{piece}|{new key}".split("|") -> ["ChangeKey", "{piece}", "{new tone}"]
+                String pieceName = command.split("\\|")[1];
+                String newTone = command.split("\\|")[2];
+
+                //нямаме такава пиеса
+                if (!piecesMap.containsKey(pieceName)) {
+                    System.out.printf("Invalid operation! %s does not exist in the collection.%n", pieceName);
+                } else {
+                    //имаме такава пиеса
+                    //пиеса -> списък с информация {composer, tone}
+                    List<String> currentInfo = piecesMap.get(pieceName);
+                    currentInfo.remove(1); //{composer}
+                    currentInfo.add(newTone); //{composer, newTone}
+                    piecesMap.put(pieceName, currentInfo);
+                    System.out.printf("Changed the key of %s to %s!%n", pieceName, newTone);
                 }
             }
-
-            System.out.println(stopsBuilder);
             command = scanner.nextLine();
         }
 
-        System.out.println("Ready for world tour! Planned stops: " + stopsBuilder);
-    }
-
-    //метод, който валидира индекс в текста stopsBuilder
-    //true -> ако индексът е валиден
-    //false -> ако индексът не е валиден
-    public static boolean isValidIndex(int index, StringBuilder builder) {
-        return index >= 0 && index <= builder.length() - 1;
-
-
+        //пиеса -> списък с информация ([0] -> композитор, [1] -> тоналност)
+        //"{Piece} -> Composer: {composer}, Key: {key}"
+        //entry.getKey() -> име на пиесата
+        //entry.getValue() -> {composer, tone}.get(0) -> composer
+        //entry.getValue() -> {composer, tone}.get(1) -> tone
+        piecesMap.entrySet() //всички записи
+                .forEach(entry -> System.out.printf("%s -> Composer: %s, Key: %s%n", entry.getKey(), entry.getValue().get(0), entry.getValue().get(1)));
     }
 
     }
